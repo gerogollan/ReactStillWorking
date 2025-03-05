7427439280353b2930bb4b72720c1d0115e9aef1
gerogollan <gerogollan11@gmail.com>
2025-03-04 21:33:53 -0300

    asd

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -18,30 +18,6 @@ export function CartProvider({ children }) {
   
   console.log(cart);
 
-  // const addProduct = (item, quantity) => {
-  //   setCart((prevCart) => {
-  //     console.log("Carrito antes de agregar:", prevCart);
-  
-  //     // Buscar si el producto ya existe en el carrito
-  //     const existingProduct = prevCart.find((prod) => prod.id === item.id);
-  
-  //     if (existingProduct) {
-  //       // Si ya existe, actualizamos su cantidad
-  //       const updatedCart = prevCart.map((prod) =>
-  //         prod.id === item.id ? { ...prod, quantity: prod.quantity + quantity } : prod
-  //       );
-  //       console.log("Carrito después de agregar cantidad:", updatedCart);
-  //       return updatedCart; // Devolvemos el carrito actualizado
-  //     } else {
-  //       // Si el producto no existe, lo agregamos
-  //       const newCart = [...prevCart, { ...item, quantity }];
-  //       console.log("Carrito después de agregar producto nuevo:", newCart);
-  //       return newCart; // Devolvemos el carrito con el nuevo producto
-  //     }
-  //   });
-  // };
-
-
    // Verificar en consola cada vez que el carrito cambia
    useEffect(() => {
      console.log("Carrito actualizado:", cart.length);
